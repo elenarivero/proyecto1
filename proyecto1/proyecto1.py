0726ad4d563bc0d046d63d3809acacee5b8ac08c
elenarivero <elena.rivero@iesnervion.es>
2025-01-06 19:59:04 +0100

    Inicio de estilos

--- proyecto1/proyecto1.py
+++ proyecto1/proyecto1.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from proyecto1.components.navbar import navbar
+from proyecto1.styles.styles import *
 from proyecto1.views.header.header import header
 from proyecto1.views.links.links import links
 from proyecto1.views.footer.footer import footer
@@ -8,15 +9,21 @@ class State(rx.State):
     pass
 
 def index()-> rx.Component:
-    return rx.vstack(
-        navbar(),
-        header(),
-        links(),
-        footer(),
-        width="100%",
-        align="center",
-        justify="center"
-    )
+    return rx.box(
+            navbar(),
+            rx.center(
+                rx.vstack(        
+                header(),
+                links(),                
+                max_width=MAX_WIDTH,
+                width="100%",
+                align="center",
+                margin_y=Spacer.BIG
+                )
+            ),            
+            footer()
+        )
+
 
 
 app = rx.App()
---
+++ proyecto1/styles/styles.py
@@ -0,0 +1,12 @@
+
+from enum import Enum
+
+# Constants
+MAX_WIDTH = "600px"
+
+# Sizes
+class Spacer(Enum):
+    SMALL = "8px",
+    DEFAULT = "16px",
+    BIG = "32px"
+
